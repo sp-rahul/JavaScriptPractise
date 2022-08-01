4718f785de8166fbef0699b05b55d5706adb22eb
Md. Ruhul Amin <48735524+sp-rahul@users.noreply.github.com>
2022-08-01 15:04:00 +0600

    Addition Substraction

---
+++ Addition.js
@@ -0,0 +1,9 @@
+var num1 = prompt("Enter 1st number : ");
+var num2 = prompt("Enter 2nd number : ");
+
+num1 = parseInt(num1);
+num2 = parseInt(num2);
+var sum = num1 + num2;
+var sub = num1 - num2;
+document.write("Addition is : " + sum +'<br/>');
+document.write("Substraction is : " + sub );
